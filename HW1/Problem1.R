9b766b0d2388fc921bc441ec2416647bbf6bfb8c
amberchen122 <amberchen2020@gmail.com>
2022-01-07 15:13:06 -0800

    Problem 1 input data
    the rawData dataframe contains required input for P1

--- HW1/Problem1.R
+++ HW1/Problem1.R
@@ -1,1 +1,5 @@
-print("Hello, world!")
+# download.file("http://files.grouplens.org/datasets/movielens/ml-100k.zip", 'ml-100k.zip") # nolint
+# unzip("ml-100k.zip") # nolint
+ml100 <- read.csv("ml-100k/u.data", header = FALSE, sep = "\t")
+colnames(ml100) <- c("user_id", "item_id", "rating", "timestamp")
+rawData <- ml100[ , c("user_id", "timestamp")]
