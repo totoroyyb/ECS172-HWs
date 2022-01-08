55d4a0cd750b096c9331e1a16d56ecaa7fcce5a9
LeiChen <leichen753@gmail.com>
2022-01-07 19:02:38 -0800

    initial commit

--- HW1/Problem1.R
+++ HW1/Problem1.R
@@ -1,5 +1,25 @@
 # download.file("http://files.grouplens.org/datasets/movielens/ml-100k.zip", 'ml-100k.zip") # nolint
 # unzip("ml-100k.zip") # nolint
+
 ml100 <- read.csv("ml-100k/u.data", header = FALSE, sep = "\t")
 colnames(ml100) <- c("user_id", "item_id", "rating", "timestamp")
 rawData <- ml100[ , c("user_id", "timestamp")]
+
+waitTimes <- function(rawData) {
+    # sort by timestamp
+    newData <- rawData[order(rawData$timestamp),]
+    individuals <- c()
+    # Iterate over each user
+    for (i in 1:max(newData$user_id)) {
+        curData <- newData[newData$user_id == i,]$timestamp
+        curLeft <- curData[2:length(curData)]
+        individuals <- c(individuals, curLeft - curData[1:length(curLeft)])
+    }
+
+    # Collectively
+    curData <- newData$timestamp
+    overall <- curData[2:length(curData)] - curData[1:length(curData) - 1]
+    return(c(individuals, overall))
+}
+
+waitTimes(rawData)
