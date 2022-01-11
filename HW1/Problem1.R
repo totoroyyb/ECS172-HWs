46aefca42a978ab9874179ccfc220195e4a23207
LeiChen <leichen753@gmail.com>
2022-01-10 21:10:04 -0800

    "mean"

--- HW1/Problem1.R
+++ HW1/Problem1.R
@@ -8,18 +8,19 @@ rawData <- ml100[ , c("user_id", "timestamp")]
 waitTimes <- function(rawData) {
     # sort by timestamp
     newData <- rawData[order(rawData$timestamp),]
-    individuals <- list()
+    individuals <- c()
     # Iterate over each user
     for (i in 1:max(newData$user_id)) {
         curData <- newData[newData$user_id == i,]$timestamp
         curLeft <- curData[2:length(curData)]
-        individuals[[length(individuals)+1]] <- c(curLeft - curData[1:length(curLeft)])
+        curMean <- mean(curLeft - curData[1:length(curLeft)])
+        individuals <- c(individuals, curMean)
     }
 
     # Collectively
     curData <- newData$timestamp
-    overall <- curData[2:length(curData)] - curData[1:length(curData) - 1]
-    return(list(individuals, overall))
+    overall <- mean(curData[2:length(curData)] - curData[1:length(curData) - 1])
+    return(c(individuals, overall))
 }
 
 a <- waitTimes(rawData)
