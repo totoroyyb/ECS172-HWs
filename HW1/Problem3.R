4478fb0a3f93cddf91712f52cfc84b9b6bc2ad01
amberchen122 <amberchen2020@gmail.com>
2022-01-12 11:51:27 -0800

    Update Problem3.R
    change data source to Hwk1.RData

--- HW1/Problem3.R
+++ HW1/Problem3.R
@@ -1,7 +1,11 @@
-# ml100kpluscovs <- getML100K() #load BuildML100KPlusCovs.R first
+# loading in TA's script
+# library(ggplot2)
+load("Hwk1.RData")
+ml100kpluscovs$genre <- paste0(ml100kpluscovs$G1, ml100kpluscovs$G2, ml100kpluscovs$G3,
+                               ml100kpluscovs$G4, ml100kpluscovs$G5, ml100kpluscovs$G6, ml100kpluscovs$G7, ml100kpluscovs$G8)
 
 plotDensities <- function(inputDF, xName, grpName) {
-    library(ggplot2)
+    
     colors <- c("antiquewhite1", "aquamarine2", "bisque1", "cadetblue1",
                 "chocolate2", "darkmagenta", "firebrick1", "darkseagreen2")
     for (i in 1:8) {
@@ -18,25 +22,5 @@ plotDensities <- function(inputDF, xName, grpName) {
     col = colors, lwd = 2)
 }
 
-prep_data <- function(data_folder = "ml-100k") {
-    movies_df <- read.table(
-        file.path("ml-100k", "u.item"),
-        header = FALSE,
-        sep = "|",
-        quote = ""
-    )
-    ml100 <- read.table(
-        file.path("ml-100k", "u.data"),
-        header = FALSE,
-        sep = "\t"
-    )
-    colnames(ml100) <- c("user_id", "item_id", "rating", "timestamp")
-    movies_df$genre <- paste0(movies_df$V6, movies_df$V7, movies_df$V8,
-    movies_df$V9, movies_df$V10, movies_df$V11, movies_df$V12, movies_df$V13)
-    ml100$genre <- lapply(ml100$item_id, function(id) movies_df$genre[id])
-    return(ml100)
-}
 
-plotDensities(prep_data(), "rating", "genre")
-
-# plotDensities(getML100K(), )
+plotDensities(ml100kpluscovs, "rating", "genre")
