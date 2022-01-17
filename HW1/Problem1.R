484b7d21b97727fc6fc63f43da3721d6a55bf12f
Yibo Yan <totoroq@outlook.com>
2022-01-17 00:56:33 -0800

    Adjust data loading style

--- HW1/Problem1.R
+++ HW1/Problem1.R
@@ -1,9 +1,11 @@
 # download.file("http://files.grouplens.org/datasets/movielens/ml-100k.zip", 'ml-100k.zip") # nolint
 # unzip("ml-100k.zip") # nolint
 
-ml100 <- read.csv("ml-100k/u.data", header = FALSE, sep = "\t")
-colnames(ml100) <- c("user_id", "item_id", "rating", "timestamp")
-rawData <- ml100[ , c("user_id", "timestamp")]
+load("Hwk1.RData")
+
+# ml100 <- read.csv("ml-100k/u.data", header = FALSE, sep = "\t")
+# colnames(ml100) <- c("user_id", "item_id", "rating", "timestamp")
+# rawData <- ml100[ , c("user_id", "timestamp")]
 
 mergeEm <- function(listOfVecs) {
     mergedVecs <- c()
@@ -31,5 +33,5 @@ waitTimes <- function(rawData) {
     return(list(individuals, overall))
 }
 
-a <- waitTimes(rawData)
+p1_result <- waitTimes(rawData)
 
