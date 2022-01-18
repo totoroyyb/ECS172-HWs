8b24e38363189791b1343cbd2f5bc70ecc9a06f5
LeiChen <leichen753@gmail.com>
2022-01-17 21:53:57 -0800

    mean rating

--- HW1/Problem3.R
+++ HW1/Problem3.R
@@ -19,27 +19,52 @@ plotDensities <- function(inputDF, xName, grpName) {
     p
 }
 
+get_mean_rating <- function(item) {
+    sub_df <- data_for_process[data_for_process$item == 1, ]
+    mean(sub_df$rating)
+    ret <- sub_df[1,]
+    ret$meanRating <- mean(sub_df$rating)
+    ret <- ret[c("item", genre_col_names, "meanRating")]
+    return(ret)
+}
+get_mean_rating(2)
+
 prep_data <- function(allow_multiple_genres = FALSE) {
     num_genres <- 19
     genre_col_names <- paste0(rep("G", num_genres), 1:num_genres)
     preinclude_cols <- c("user", "item", "rating")
     data_for_process <- ml100kpluscovs[, c(preinclude_cols, genre_col_names)]
+    
+    get_mean_rating <- function(movie_num) {
+        sub_df <- data_for_process[data_for_process$item == movie_num, ]
+        mean(sub_df$rating)
+        ret <- sub_df[1,]
+        ret$meanRating <- mean(sub_df$rating)
+        ret <- ret[c("item", genre_col_names, "meanRating")]
+        return(ret)
+    }
+
+    mean_df <- data.frame()
+    for (i in unique(data_for_process$item)) {
+        mean_df <- rbind(mean_df, get_mean_rating(i))
+    }
 
     convert_genre <- function(r) {
         genre_name <- sample(names(r)[r == 1], 1)
         genre_index <- substr(genre_name, 2, nchar(genre_name))
         as.integer(genre_index)
     }
+    
 
     converted_genres <- apply(
-        data_for_process[, genre_col_names],
+        mean_df[, genre_col_names],
         1,
         convert_genre
     )
-    cbind(data_for_process, genre = converted_genres)
+    cbind(mean_df, genre = converted_genres)
 }
 
 genre_df <- prep_data()
 print("Only plotting first 8 genres for clarity...")
 genre_df <- genre_df[genre_df["genre"] <= 8, ]
-plotDensities(genre_df, "rating", "genre")
+plotDensities(genre_df, "meanRating", "genre")
