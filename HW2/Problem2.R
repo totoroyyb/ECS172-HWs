23d762f7c873113654de3723c4985854bd3aadd2
amberchen122 <amberchen2020@gmail.com>
2022-01-31 16:35:36 -0800

    給housing data column改個名字

--- HW2/Problem2.R
+++ HW2/Problem2.R
@@ -1,2 +1,25 @@
 load('Hwk2.RData') #generation code is in the data folder
 require(rectools)
+require(qeML)
+
+# data exploration
+
+#check for NA values -> no NA values
+which(is.na(house_vote), arr.ind=TRUE)
+which(is.na(InstEval), arr.ind=TRUE)
+which(is.na(ml100kpluscovs), arr.ind=TRUE)
+
+# data processing
+
+
+# CF model
+set.seed(9999)
+
+movieLin0 <- qeLin(ml100kpluscovs[,c('user','item','rating')], 'rating')
+cat('base accuracy: ', movieLin$baseAcc) 
+cat('test accuracy: ', movieLin$testAcc)
+
+movieLin1 <- qeLin(ml100kpluscovs[,c('user','item', 'age', 'rating')], 'rating')
+
+# house_vote
+# movieLin2 <- qeLogit(house_vote[,c('party','', 'rating')], 'rating')
--- HW2/data/Problem2_data_generation.R
+++ HW2/data/Problem2_data_generation.R
@@ -1,6 +1,29 @@
-load('Hwk1.RData')
 require(rectools)
+
+#InstEval
 getInstEval()
-colnames<- c('party', 'handicapped-infants', 'water-project-cost-sharing', 'adoption-of-the-budget-resolution', 'physician-fee-freeze', 'el-salvador-aid', 'religious-groups-in-schools', 'anti-satellite-test-ban', 'aid-to-nicaraguan-contras', 'mx-missile', 'immigration', 'synfuels-corporation-cutback', 'education-spending', 'superfund-right-to-sue', 'crime', 'duty-free-exports', 'export-administration-act-south-africa')
+
+#house_vote
+colnames<- c('party', paste0(rep("B", 16), 1:16))
 house_vote = read.csv('house-votes-84.data', header = FALSE, col.names = colnames)
+
+
+
+
+#ml100kpluscovs
+load('Hwk1.RData')
+genre_col_names <- paste0(rep("G", 19), 1:19)
+convert_genre <- function(r) {
+  genre_name <- sample(names(r)[r == 1], 1)
+  genre_index <- substr(genre_name, 2, nchar(genre_name))
+  as.integer(genre_index)
+}
+converted_genres <- apply(
+  ml100kpluscovs[, genre_col_names],
+  1,
+  convert_genre
+)
+
+
+
 save(ml100kpluscovs, house_vote, InstEval, file = 'Hwk2.RData')
