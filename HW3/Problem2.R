66d6813044cb5080c43401f8c08f9780f8d2fd1d
Yibo Yan <totoroq@outlook.com>
2022-02-15 01:06:02 -0800

    prepare the dataset for hw3 p2

--- HW3/Problem2.R
+++ HW3/Problem2.R
@@ -1,1 +1,10 @@
+prep_data <- function(data_dir = "./data/") {
+    url <- "http://www2.informatik.uni-freiburg.de/~cziegler/BX/BX-CSV-Dump.zip"
+    temp <- tempfile()
+    download.file(url, dest = temp)
+    unzip(temp, exdir = data_dir)
+}
+
+# prep_data()
+
 
