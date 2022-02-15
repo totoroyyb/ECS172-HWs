ea93a2e67b5337a9a1ea48cd4be50d4ce1ed6040
Wilson0925 <54805745+Wilson0925@users.noreply.github.com>
2022-02-14 18:03:46 -0800

    Analysis pef Dataset

--- HW3/Problem1.R
+++ HW3/Problem1.R
@@ -2,6 +2,7 @@ require(regtools)
 require(rectools)
 require(qeML)
 data(mlb)
+data(pef)
 mlb <- mlb[, 3:6]
 mlbd <- factorsToDummies(mlb, omitLast = T)
 mlbd <- as.data.frame(mlbd)
@@ -36,3 +37,14 @@ smallestMAPE <- function(feature_MAPE) {
 feature_MAPE = lmAlpha(mlbd, "Weight", 50, 250)
 print(feature_MAPE)
 print(smallestMAPE(feature_MAPE))
+
+# Analysis pef Dataset
+pef <- pef[, c(1, 3:6)]
+pefd <- factorsToDummies(pef, omitLast = T)
+pefd <- as.data.frame(pefd)
+feature_MAPE_pef = lmAlpha(pefd, "wageinc", 50, 250)
+print(feature_MAPE_pef)
+print(smallestMAPE(feature_MAPE_pef))
+
+
+
