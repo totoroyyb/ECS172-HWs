5a1a3b8b76c3169461416364b776fd7869044593
Shujuan Chen <63131314+amberchen122@users.noreply.github.com>
2022-02-22 23:33:54 -0500

    Update Problem2.R
    from: https://stackoverflow.com/questions/14708140/how-to-transform-a-list-of-user-ratings-into-a-matrix-in-r
    
    ??useful???? i dont understand the resulting df

--- HW3/Problem2.R
+++ HW3/Problem2.R
@@ -1,3 +1,4 @@
+library(data.table)
 prep_data <- function(data_dir = "./data/") {
     url <- "http://www2.informatik.uni-freiburg.de/~cziegler/BX/BX-CSV-Dump.zip"
     temp <- tempfile()
@@ -5,6 +6,20 @@ prep_data <- function(data_dir = "./data/") {
     unzip(temp, exdir = data_dir)
 }
 
+
+get_dataF <- function() {
+  Users <- read_delim("data/BX-Users.csv", 
+                         delim = ";", escape_double = FALSE, trim_ws = TRUE)
+  Books <- read_delim("data/BX-Books.csv", 
+                         delim = ";", escape_double = FALSE, trim_ws = TRUE)
+  Ratings <- read_delim("data/BX-Book-Ratings.csv", 
+                         delim = ";", escape_double = FALSE, trim_ws = TRUE)
+  save(Books, Ratings, Users, file = './data/Hwk3.RData')
+}
+
 # prep_data()
+# get_dataF()
+User_ID = as.matrix(unique(Ratings$`User-ID`))
+Book_ID =  as.matrix(unique(Ratings$`ISBN`))
 
-
+Ratings_matrix <- matrix(Ratings$`Book-Rating`, nrow = length(unique(Ratings$`User-ID`)), byrow = TRUE)
