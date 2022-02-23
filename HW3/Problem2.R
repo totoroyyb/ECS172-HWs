e624b9475ffee1fc91b871da5dff0aa49dede20f
Yibo Yan <totoroq@outlook.com>
2022-02-23 11:40:55 -0800

    add file

--- HW3/Problem2.R
+++ HW3/Problem2.R
@@ -6,20 +6,26 @@ prep_data <- function(data_dir = "./data/") {
     unzip(temp, exdir = data_dir)
 }
 
+options(warn = -1)
+Users <- fread('./data/BX-Users.csv', sep=';')
+Books<- fread('./data/BX-Books.csv', sep=';')
+Books_Ratings <- fread('./data/BX-Book-Ratings.csv', sep=';')
+options(warn = 0)
+Rating_Matrix <- matrix(, nrow = dim(Users)[1], ncol = dim(Books)[1])
 
-get_dataF <- function() {
-  Users <- read_delim("data/BX-Users.csv", 
-                         delim = ";", escape_double = FALSE, trim_ws = TRUE)
-  Books <- read_delim("data/BX-Books.csv", 
-                         delim = ";", escape_double = FALSE, trim_ws = TRUE)
-  Ratings <- read_delim("data/BX-Book-Ratings.csv", 
-                         delim = ";", escape_double = FALSE, trim_ws = TRUE)
-  save(Books, Ratings, Users, file = './data/Hwk3.RData')
-}
+# get_dataF <- function() {
+#   Users <- read_delim("data/BX-Users.csv", 
+#                          delim = ";", escape_double = FALSE, trim_ws = TRUE)
+#   Books <- read_delim("data/BX-Books.csv", 
+#                          delim = ";", escape_double = FALSE, trim_ws = TRUE)
+#   Ratings <- read_delim("data/BX-Book-Ratings.csv", 
+#                          delim = ";", escape_double = FALSE, trim_ws = TRUE)
+#   save(Books, Ratings, Users, file = './data/Hwk3.RData')
+# }
 
 # prep_data()
 # get_dataF()
-User_ID = as.matrix(unique(Ratings$`User-ID`))
-Book_ID =  as.matrix(unique(Ratings$`ISBN`))
+# User_ID <- as.matrix(unique(Ratings$`User-ID`))
+# Book_ID <-  as.matrix(unique(Ratings$`ISBN`))
 
-Ratings_matrix <- matrix(Ratings$`Book-Rating`, nrow = length(unique(Ratings$`User-ID`)), byrow = TRUE)
+# Ratings_matrix <- matrix(Ratings$`Book-Rating`, nrow = length(unique(Ratings$`User-ID`)), byrow = TRUE)
