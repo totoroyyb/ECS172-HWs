266eb7d794ff0d9aac897872137778bd1a7cd6bb
Wilson0925 <54805745+Wilson0925@users.noreply.github.com>
2022-03-14 13:20:04 -0700

    Code Finished

--- TermProject/TermProject.R
+++ TermProject/TermProject.R
@@ -3,39 +3,38 @@ require(qeML)
 require(ggplot2)
 library(recosystem)
 load("./data/ml100kpluscovs.RData")
+load("./data/InstEval.RData")
 
 # Create The Fake Rating Matrix By Linear Model
-user_mean <- tapply(ml100kpluscovs$userMean, ml100kpluscovs$user, mean)
-item_mean <- tapply(ml100kpluscovs$itemMean, ml100kpluscovs$item, mean)
+generate_fake_rating_matrix <- function(data_frame) {
+    user_mean <- tapply(data_frame$userMean, data_frame$user, mean)
+    item_mean <- tapply(data_frame$itemMean, data_frame$item, mean)
 
-model <- qeLin(ml100kpluscovs[, c("rating", "userMean", "itemMean")], "rating")
-user_mean_coef <- matrix(rep(user_mean, each = length(item_mean)),
-ncol = length(item_mean), byrow = TRUE) * model$coefficients["userMean"]
-item_mean_coef <- t(matrix(rep(item_mean, each = length(user_mean)),
-ncol = length(user_mean), byrow = TRUE)) * model$coefficients["itemMean"]
+    model <- qeLin(data_frame[, c("rating", "userMean", "itemMean")],
+    "rating")
+    user_mean_coef <- matrix(rep(user_mean, each = length(item_mean)),
+    ncol = length(item_mean), byrow = TRUE) * model$coefficients["userMean"]
+    item_mean_coef <- t(matrix(rep(item_mean, each = length(user_mean)),
+    ncol = length(user_mean), byrow = TRUE)) * model$coefficients["itemMean"]
 
-rating_matrix <- user_mean_coef + item_mean_coef +
-model$coefficients["(Intercept)"]
+    rating_matrix <- user_mean_coef + item_mean_coef +
+    model$coefficients["(Intercept)"]
 
-for (index in seq_len(length(ml100kpluscovs$user))) {
-    rating_matrix[ml100kpluscovs$user[index], ml100kpluscovs$item[index]] <-
-    ml100kpluscovs$rating[index]
+    colnames(rating_matrix) <- seq_len(length(item_mean))
+    rownames(rating_matrix) <- seq_len(length(user_mean))
+
+    for (index in seq_len(length(data_frame$user))) {
+        rating_matrix[data_frame$user[index], data_frame$item[index]] <-
+        data_frame$rating[index]
+    }
+
+    return(list(rating_matrix, user_mean, item_mean))
 }
-
-# Set Up Choices of n(Number of Users), m(Number of Items), d(non-NA Density)
-set.seed(123)
-n <- c(150, 300, 450, 600, 750, length(user_mean))
-m <- c(300, 600, 900, 1200, length(item_mean))
-d <- c(20, 40, 60, 80, 100)
-
-# Create Folders for Saving Plots
-dir.create("./results", showWarnings = FALSE)
-dir.create("./results/MovieLens", showWarnings = FALSE)
-dir.create("./results/MovieLens/Linear Model", showWarnings = FALSE)
-dir.create("./results/MovieLens/MF (Reco)", showWarnings = FALSE)
 
 # Generate the Required Subset Rating Matrix
 generate_rating_matrix_subset <- function(n, m, d, rating_matrix) {
+    orig_row_names <- rownames(rating_matrix)
+    orig_col_names <- colnames(rating_matrix)
     rating_matrix_subset <- rating_matrix[1:n, 1:m]
 
     rating_matrix_subset <- array(rating_matrix_subset)
